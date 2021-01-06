eeb257182a291d5bc28bde47eef8f0ccd9e8fb7c
Wolfgang Schildbach <wschildbach@fermi.franken.de>
2021-01-06 07:20:52 +0100

    first version of vessel POST

--- tracksrv/tracks/views.py
+++ tracksrv/tracks/views.py
@@ -15,9 +15,16 @@ def index(request):
     return JsonResponse(trackList, safe=False)
 
 def detail(request, track_id):
-    track = get_object_or_404(Track, pk=track_id)
-    trackDetail = {'id':track.id,'uploaded_on':track.uploaded_on}
-    return JsonResponse(trackDetail)
+    if request.method == 'POST':
+        vessel_id = request.GET.get('vessel')
+        submitter_id = 1 # derive from logged-in user
+        vessel = Vessel.object.get(pk=vessel_id)
+        track = Track(vessel=vessel, submitter=submitter_id)
+        track.save()
+    else:
+        track = get_object_or_404(Track, pk=track_id)
+        trackDetail = {'id':track.id,'uploaded_on':track.uploaded_on}
+        return JsonResponse(trackDetail)
 
 def raw(request, track_id):
     track = get_object_or_404(Track, pk=track_id)
