cbc01c907b71d33f49cd5002c808be3d8d7a7373
Wolfgang Schildbach <wschildbach@fermi.franken.de>
2021-01-07 22:04:29 +0100

    add number of points to track detail view

--- tracksrv/tracks/views.py
+++ tracksrv/tracks/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponse,JsonResponse
 from django.core.paginator import Paginator
 
-from .models import Track
+from .models import Track,Sounding
 
 def index(request):
     page = request.GET.get('page',default=1) # XXX use next/prev style navigation
@@ -23,7 +23,7 @@ def detail(request, track_id):
         track.save()
     else:
         track = get_object_or_404(Track, pk=track_id)
-        trackDetail = {'id':track.id,'uploaded_on':track.uploaded_on}
+        trackDetail = {'id':track.id,'uploaded_on':track.uploaded_on,'n_soundings':Sounding.objects.filter(track=track).count()}
         return JsonResponse(trackDetail)
 
 def raw(request, track_id):
