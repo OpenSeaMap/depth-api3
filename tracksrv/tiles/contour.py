856af4145266b6742729011752851061a4bbdacc
Wolfgang Schildbach <wschildbach@fermi.franken.de>
2021-01-09 22:35:08 +0100

    implement tile contour rendering

---
+++ tracksrv/tiles/contour.py
@@ -0,0 +1,107 @@
+import io
+import threading
+import numpy as np
+
+from scipy.interpolate import griddata
+
+from matplotlib.figure import Figure
+from matplotlib.axes   import Axes
+from matplotlib.colors import Normalize, ListedColormap
+
+"""
+We want to map depth measurements to colors. 
+
+    0-1 m dark blau (0.47,0.67,0.90)
+    1-4 m medium blue (0.6,0.80,1)
+    4-6 m light blue (0.8,1,1)
+    6+ m white
+    for reference: land -- ffffcc, buildings: e4e4b1
+
+0   1   4   6  50
+
+ B3  B2  B1  WHT  (transparent below 50 m)
+
+0   1   2   3   4
+0  .25 .50 .75  1
+"""
+# a standard colorscheme for the depths
+std_chart_colors = {"levels":(0,1,4,6,50), "colors": ((0.47,0.67,0.9), (0.6,0.8,1.0), (0.8,1.0,1.0), (1.0,1.0,1.0))}
+
+class seabed_norm(Normalize):
+    """Normalization of depth values into sea bed colormap
+
+    """
+    def __init__(self,levels):
+        Normalize.__init__(self)
+        self.x,self.y = levels,np.linspace(0,1,num=len(levels))
+
+    def __call__(self,value,clip=None):
+        return np.ma.masked_array(np.interp(value,self.x,self.y))
+
+tileLock = threading.Lock()
+
+EQUATOR = 40075016.68557849
+
+def tile_to_3857(z,x,y):
+  tpz = 2**z
+  x =  x/tpz*EQUATOR - EQUATOR/2
+  y = -y/tpz*EQUATOR + EQUATOR/2
+  return x,y
+
+def tile(z,xi,yi,c_tile,depth,res=256,fmt='png',colormap=True,soundings=None,isolines=None,color_scheme=std_chart_colors):
+  """return a graphics file with contour lines
+  z -- zoom
+  x,y -- tile coordinates
+  res=256,fmt='png',colormap=False,soundings=True,isolines=True,color_scheme=std_chart_colors
+  """
+
+  if soundings is None:
+    soundings = (z>=14)
+  if isolines is None:
+    isolines = (z>=12)
+
+  # controls quality: higher = better. No need to set higher than res/2
+  subdiv = 128
+
+  # protect against multiple threads running in here
+  tileLock.acquire()
+
+  fig = Figure(figsize=[1.,1.],frameon=False)
+  ax = Axes(fig, [0,0,1,1],frameon=False,clip_on=True)
+  ax.set_axis_off()
+  ax.set_autoscale_on(False)
+  x0,y0,x1,y1 = *tile_to_3857(z,xi,yi),*tile_to_3857(z,xi+1,yi+1)
+  ax.set_xbound(x0,x1)
+  ax.set_ybound(y0,y1)
+  fig.add_axes(ax)
+
+  cm_seabed = ListedColormap(color_scheme["colors"])
+  sn = seabed_norm(color_scheme["levels"])
+
+  if (colormap or isolines) and len(depth) >= 3:
+
+      xs, ys = np.mgrid[x0:x1:subdiv*1j, y0:y1:subdiv*1j]
+
+      grid_d = griddata(c_tile, depth, (xs,ys), method='linear')
+
+      if colormap:
+          cset = ax.contourf(xs, ys, grid_d, levels=color_scheme["levels"], cmap=cm_seabed, norm=sn),
+
+      if isolines:
+
+          # start with non-labeled contour/s
+          ax.contour(xs, ys, grid_d, levels=(15,),colors='b',linewidths=0.25),
+
+          # then, labeled contours
+          ax.clabel(
+              ax.contour(xs, ys, grid_d, levels=(10,20,30),colors='b',linewidths=0.25),
+          inline=1, fontsize=5, fmt="%1.0f")
+
+  output = io.BytesIO()
+  fig.savefig(output,pad_inches=0,bbox_inches='tight',dpi=res,transparent=True,format=fmt)
+  contents = output.getvalue()
+  output.close()
+
+  tileLock.release()
+
+  return contents
--- tracksrv/tiles/views.py
+++ tracksrv/tiles/views.py
@@ -10,15 +10,17 @@ from scipy.interpolate import griddata
 from django.shortcuts import render
 from django.http import FileResponse, HttpResponse, JsonResponse
 from django.contrib.gis.geos import GEOSGeometry, Polygon
+from django.utils.cache import patch_response_headers
 
-import tiles.transform as tf
+#import tiles.transform as tf
+import tiles.contour
 from tracks.models import Track,Sounding
 
 #import redis
 
 # initialize a global process pool
-cpus = len(sched_getaffinity(0))
-processPool = Pool(processes = max(1,cpus-1))
+#cpus = len(sched_getaffinity(0))
+#processPool = Pool(processes = max(1,cpus-1))
 
 class Perf():
     def __init__(self):
@@ -27,38 +29,55 @@ class Perf():
     def done(self):
         return time.time()-self.start
 
+EQUATOR = 40075016.68557849
+
+def tile_to_3857(z,x,y):
+  tpz = 2**z
+  x =  x/tpz*EQUATOR - EQUATOR/2
+  y = -y/tpz*EQUATOR + EQUATOR/2
+  return x,y
+
 def contour(request,z,x,y):
     """
     return a tile with contour lines
     """
 
-    p = Perf()
+    perf = Perf()
 
-    tpz = 2 ** z # two to (the power of) zoom
+    bbox = Polygon.from_bbox((*tile_to_3857(z,x-1,y+2), *tile_to_3857(z,x+2,y-1)))
+    bbox.srid = 3857
 
-    #    boxcoords = "(({},{}),({},{}))".format(x2lon(tpz,xi-1),y2lat(tpz,yi-1),x2lon(tpz,xi+2),y2lat(tpz,yi+2))
+#    print(bbox)
 
-    bbox = Polygon.from_bbox((tf.x2lon(tpz,x-1),tf.y2lat(tpz,y-1),tf.x2lon(tpz,x+2),tf.y2lat(tpz,y+2)))
-    pts = Sounding.objects.filter(min_level__lte=z, coord__coveredby=bbox)
+    pts = Sounding.objects.filter(min_level__lte=z, coord__contained=bbox)
     npts = pts.count()
+    msg = """we discovered %d points in %f ms
+    """%(npts,1000*perf.done())
     if npts > 0:
         c_tile = np.empty((npts,2),float)
-        depth  = np.empty((npts,1),float)
 
-        i = 0
-        for p in pts:
-            c_tile[i,0] = tf.lon2x(tpz,p.coord[0])
-            c_tile[i,1] = tf.lat2y(tpz,p.coord[1])
-            depth[i]    = p.coord[2]
+        c_tile[:,0] = np.fromiter((p.coord.transform(3857,clone=True).x for p in pts), float)
+        c_tile[:,1] = np.fromiter((p.coord.transform(3857,clone=True).y for p in pts), float)
+        depth       = np.fromiter((p.coord.z for p in pts),float)
 
-    msg = "we discovered %d points in %f ms"%(npts,p.done())
-    print(msg)
+        msg += "we discovered and copied %d points in %f ms"%(npts,1000*perf.done())
+        print(msg)
+#        print()
 
-    return HttpResponse(msg)
+#        print(pts[0].coord)
+#        print(c_tile)
+#        print(depth)
 
-    return FileResponse(
-        io.bytesIO(s),
-        mimetype='image/png',
-        last_modified = datetime.fromtimestamp(float(redis.hget(key,'last_modified'))),
-        cache_timeout = tileRender.EXPIRE_SECS, # will be max_age in newer versions of flask
-        as_attachment=False)
+#        return HttpResponse(msg)
+
+        s = tiles.contour.tile(z,x,y,c_tile,depth)
+    else:
+        return HttpResponse("")
+
+    fr = FileResponse(
+        io.BytesIO(s),
+        as_attachment = False)
+    fr['Content-Type'] = 'image/png'
+    patch_response_headers(fr,30)
+    
+    return fr
