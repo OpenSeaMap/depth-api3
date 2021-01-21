391c12a95e52826ed6f2f7bdea1a5975c3be5b7e
Wolfgang Schildbach <wschildbach@fermi.franken.de>
2021-01-21 12:16:10 +0100

    make more fields optional in model

--- tracksrv/tracks/models.py
+++ tracksrv/tracks/models.py
@@ -23,10 +23,10 @@ class Vessel(models.Model):
     length = models.FloatField('length in meters')
     beam = models.FloatField('beam in meters')
     draft = models.FloatField('draft in meters')
-    height = models.FloatField('height in meters', null=True)
+    height = models.FloatField('height in meters')
     displacement = models.FloatField('displacement in metric tons')
-    manufacturer = models.CharField('the name of the manufacturer',max_length=128)
-    model = models.CharField('the model/type of vessel as given by the manufacturer',max_length=128)
+    manufacturer = models.CharField('the name of the manufacturer',blank=True,max_length=128)
+    model = models.CharField('the model/type of vessel as given by the manufacturer',blank=True,max_length=128)
     vtype = models.CharField(max_length=2, choices=VesselType.choices)
     depth_sensor_offset_x = models.FloatField('distance of transducer from stern in meters')
     depth_sensor_offset_y = models.FloatField('distance of transducer from centerline in meters')
@@ -35,9 +35,9 @@ class Vessel(models.Model):
     gps_sensor_offset_y = models.FloatField('longitudinal offset of GPS sensor in meters')
     gps_sensor_offset_z = models.FloatField('position above waterline in meters')
     measurement_type = models.CharField(max_length=2, null=True, choices=MeasurementType.choices)
-    depth_sensor_offset_keel = models.FloatField(null=True) # XXX what is this?
-    depth_sensor_manufacturer = models.CharField(max_length=32, null=True)
-    depth_sensor_model = models.CharField(max_length=20, null=True)
+    depth_sensor_offset_keel = models.FloatField(null=True, blank=True) # XXX what is this?
+    depth_sensor_manufacturer = models.CharField(max_length=32, blank=True)
+    depth_sensor_model = models.CharField(max_length=20, blank=True)
     def __str__(self):
         return '%s %s (%s %s) (%2.1fm/%1.1fm/%1.1fm/%1.1ft)'%(Vessel.VesselType(self.vtype).label,self.name,self.manufacturer,self.model,self.length,self.beam,self.draft,self.displacement)
 
@@ -64,11 +64,11 @@ class Track(models.Model):
     vessel = models.ForeignKey(Vessel, on_delete=models.CASCADE)
     submitter = models.ForeignKey(User, on_delete=models.CASCADE)
     uploaded_on = models.DateTimeField('date uploaded',default=timezone.now)
-    processing_status = models.CharField(max_length=3, null=True, choices=ProcessingStatus.choices, default=ProcessingStatus.NEW)
+    processing_status = models.CharField(max_length=3, blank=True, choices=ProcessingStatus.choices, default=ProcessingStatus.NEW)
     rawfile = models.FileField(upload_to='raw_tracks/')
-    format = models.CharField(max_length=3,null=True,choices=FileFormat.choices,default=None)
-    note = models.CharField('optional uploaders\' note',max_length=200,null=True, default=None)
-    quality = models.IntegerField('a track quality measure from 0 (unusable) to 100 (perfect)', null=True, default=None)
+    format = models.CharField(max_length=3,blank=True,choices=FileFormat.choices)
+    note = models.CharField('optional uploaders\' note',max_length=200,blank=True)
+    quality = models.IntegerField('a track quality measure from 0 (unusable) to 100 (perfect)', blank=True)
     def __str__(self):
         return '[Track %d] (on %s %s, submitted by %s on %s)' % (self.id,Vessel.VesselType(self.vessel.vtype).label,self.vessel.name,str(self.submitter),self.uploaded_on)
 
