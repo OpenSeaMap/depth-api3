76b2cbdc01bd4416ec9dde1ae79af69b52d61a55
Wolfgang Schildbach <wschildbach@fermi.franken.de>
2021-02-02 20:13:05 +0100

    enable DepthUser and ProcessingStatus in admin UI

--- tracksrv/tracks/admin.py
+++ tracksrv/tracks/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Track,Vessel
+from .models import Track,Vessel,DepthUser,ProcessingStatus
 
 admin.site.register(Track)
 admin.site.register(Vessel)
+admin.site.register(DepthUser)
+admin.site.register(ProcessingStatus)
