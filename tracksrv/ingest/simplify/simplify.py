966022d1e8c0e1b8eabec761c21a61351df55f06
Wolfgang Schildbach <wschildbach@fermi.franken.de>
2021-02-05 18:46:05 +0100

    rewrite simplifier into OOP

--- tracksrv/ingest/simplify/simplify.py
+++ tracksrv/ingest/simplify/simplify.py
@@ -30,57 +30,99 @@ from tracks.models import Track,Sounding,ProcessingStatus
 import tiles.transform as tf
 from tiles.util import tile_to_3857,Perf,Stat
 
-def minTile(context,z,x,y):
 
-  def q(z,x,y):
+class HierachicalSimplifier:
+
+  # the maximum number of points that should be left in a tile
+  MAX_POINTS = 256
+
+  # recurse, at a minimum, to this level
+  MIN_RECURSE = 15
+
+  def __init__(self,max_recurse,track=None):
+    self.max_recurse = max(self.MIN_RECURSE,max_recurse)
+    self.track = track
+    self.allLookup = [Stat() for x in range(max_recurse+1)]
+    self.oneLookup = [Stat() for x in range(max_recurse+1)]
+    self.allUpdate = [Stat() for x in range(max_recurse+1)]
+    self.oneUpdate = [Stat() for x in range(max_recurse+1)]
+    # XXX TODO adapt for no-track simplification
+    self.progress  = ProcessingStatus(name="simp -m %d %s"%(max_recurse,0),
+                                      track = track or Track.objects.get(id=1),
+                                      toProcess=self.q(0,0,0,track).count())
+    self.progress.save()
+
+  def run(self):
+    self.minTile(0,0,0)
+    self.progress.end()
+
+  @staticmethod
+  def q(z,x,y,track):
     bbox = Polygon.from_bbox((*tile_to_3857(z,x,y+1), *tile_to_3857(z,x+1,y)))
     bbox.srid = 3857
-    return Sounding.objects.filter(track=track, coord__coveredby=bbox)
+    q = Sounding.objects.filter(coord__coveredby=bbox)
+    if track is not None:
+      q = q.filter(track=track)
+    return q
 
-  def printProg(z,x,y,s):
-    print(' '*(150),end='\r')
-    print('z=%d, x=%d, y=%d %s'%(z,x,y,s),end='\r')
+  def minTile(self,z,x,y):
 
-  with Perf() as p:
-    pts = q(z,x,y)
-    haspts = pts.exists()
-  context['stat1'][z].add(p.t)
 
-  if not haspts:
-    return None
-
-  if (z < 15 or pts.count() > 4**context['subdiv']) and z < context['maxlev']+context['subdiv']:
-    c = [[2*x,2*y],[2*x+1,2*y],[2*x,2*y+1],[2*x+1,2*y+1]]
-
-    # determine minimal sounding
-    # XXX could smarten up when we update the database
-    minimal = None
-    for xx,yy in c:
-      r = minTile(context,z+1,xx,yy)
-      if r is not None:
-        if (minimal is None) or (r['z'] < minimal['z']):
-          minimal = r
-
-  else:
-    context['ps'].incProgress(pts.count())
-    with Perf() as p:
-      minimal = pts.annotate(mz=Min('z')).filter(mz=F('z')).values('id','z')[0]
-    context['stat2'][z].add(p.t)
+    def printProg(z,x,y):
+      print(' '*(150),end='\r')
+      print('z=%d, x=%d, y=%d %s'%(z,x,y,self.progress),end='\r')
 
     with Perf() as p:
-      pts.exclude(min_level=z+1).update(min_level=z+1)
-    context['stat3'][z].add(p.t)
+      pts = self.q(z,x,y,self.track)
+    self.allLookup[z].add(p.t)
 
-  # we now have minimal
-  with Perf() as p:
-    Sounding.objects.filter(id=minimal['id']).exclude(min_level=z-context['subdiv']).update(min_level=z-context['subdiv'])
-  # we could stop the updating here -- if no change, or rather -- change here increases the min_level
-  context['stat4'][z].add(p.t)
+    if not pts.exists():
+      return None
 
-  if z == 15:
-    printProg(z,x,y,str(context['ps']))
+    # determine whether to recurse
+    if z < self.MIN_RECURSE:
+      recurse = True
+    else:
+      # if we are beyond MIN_RECURSE, but below max_recurse, go check how many points in total are contained
+      # if fewer than MAX_POINTS, break the recursion
+      if self.track is not None:
+        pts = self.q(z,x,y,self.track)
+      recurse = (z < self.max_recurse) and (pts.count() > self.MAX_POINTS)
 
-  return minimal
+    if recurse:
+      pts = None # discard pts; we will not need this anymore
+      c = [[2*x,2*y],[2*x+1,2*y],[2*x,2*y+1],[2*x+1,2*y+1]]
+
+      # determine minimal sounding
+      # XXX could smarten up when we update the database
+      # we could also consider randomizing the order, such that we get more even progress
+      minimal = None
+      for xx,yy in c:
+        r = self.minTile(z+1,xx,yy)
+        if r is not None:
+          if (minimal is None) or (r['z'] < minimal['z']):
+            minimal = r
+
+    else:
+      self.progress.incProgress(pts.count())
+      with Perf() as p:
+        minimal = pts.annotate(mz=Min('z')).filter(mz=F('z')).values('id','z')[0]
+      self.oneLookup[z].add(p.t)
+
+      with Perf() as p:
+        pts.exclude(min_level=z+1).update(min_level=z+1)
+      self.allUpdate[z].add(p.t)
+
+    # we now have minimal
+    with Perf() as p:
+      Sounding.objects.filter(id=minimal['id']).exclude(min_level=z).update(min_level=z)
+    self.oneUpdate[z].add(p.t)
+    # we could stop the updating here -- if no change, or rather -- change here increases the min_level
+
+    if z == 14:
+      printProg(z,x,y)
+
+    return minimal
 
 def printStat(title,stat):
   def r(h,c,hh=True):
@@ -103,34 +145,19 @@ def printStat(title,stat):
   r('ctm',[x.c for x in stat])
   r('atm',[1000*x.avg() for x in stat],False)
 
-def simplifyFull(track,subdiv,maxlev):
-  context={'subdiv':subdiv,'maxlev':maxlev}
-  context['stat1'] = [Stat() for x in range(maxlev+subdiv+1)]
-  context['stat2'] = [Stat() for x in range(maxlev+subdiv+1)]
-  context['stat3'] = [Stat() for x in range(maxlev+subdiv+1)]
-  context['stat4'] = [Stat() for x in range(maxlev+subdiv+1)]
+def simplifyFull(max_level,track=None):
+  simplifier = HierachicalSimplifier(max_level,track)
+  simplifier.run()
 
-  context['ps'] = ProcessingStatus(name="simp -s %d -m %d"%(subdiv,maxlev),
-                                    track=track,
-                                    toProcess=track.nPoints)
-  context['ps'].save()
-
-  minTile(context,0,0,0)
-
-  context['ps'].end()
-
-  printStat('all-search',context['stat1'])
-  printStat('min-search',context['stat2'])
-  printStat('all-update',context['stat3'])
-  printStat('one-update',context['stat4'])
-
+  printStat('all-search', simplifier.allLookup)
+  printStat('min-search', simplifier.oneLookup)
+  printStat('all-update', simplifier.allUpdate)
+  printStat('one-update', simplifier.oneUpdate)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Process soundings to limit number of soundings per tile.')
   parser.add_argument('-f', '--force', dest='force', action='store_true',
                       help='process track even if it has already been processed')
-  parser.add_argument('-s', '--subdiv', dest='subdiv', action='store', type=int, default=2,
-                      help='subdivision level')
   parser.add_argument('-m', '--maxlevel', dest='maxlev', action='store', type=int, default=Sounding.MAX_LEVEL,
                       help='maximum level')
   parser.add_argument('tracks', metavar='i', type=int, nargs='*',
@@ -138,18 +165,19 @@ if __name__ == "__main__":
 
   args = parser.parse_args()
 
-  tracks = Track.objects.exclude(sounding=None)
-  if not args.force:
-    tracks = tracks.annotate(minlev=Min('sounding__min_level')).exclude(minlev__lt=Sounding.MAX_LEVEL+1)
+  if len(args.tracks) == 0:
+    logger.info("simplify all")
+    with Perf() as p:
+      simplifyFull(args.maxlev)
+    logger.info("\nsimplification took %f s",p.t)
+  else:
 
-  if len(args.tracks) > 0:
-    tracks = tracks.filter(id__in=args.tracks)
+    tracks = Track.objects.exclude(sounding=None,id__in=args.tracks)
+    if not args.force:
+      tracks = tracks.annotate(minlev=Min('sounding__min_level')).exclude(minlev__lt=Sounding.MAX_LEVEL+1)
 
-  if tracks.count() > 0:
-    print("Processing tracks: %s"%[t.id for t in tracks])
-
-    for track in tracks:
-      logger.info("simplifying track %s",str(track))
-      with Perf() as p:
-        simplifyFull(track, args.subdiv,args.maxlev)
-      logger.info("\nsimplification took %f s",p.t)
+      for track in tracks:
+        logger.info("simplifying track %s",str(track))
+        with Perf() as p:
+          simplifyFull(args.maxlev, track)
+        logger.info("\nsimplification took %f s",p.t)
