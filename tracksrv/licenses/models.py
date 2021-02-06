ba923d42a3829d5b3d2fbbe84c41bcb7d54b7e58
Wolfgang Schildbach <wschildbach@fermi.franken.de>
2021-02-06 12:23:51 +0100

    add version and visited_on fields to license model

--- tracksrv/licenses/models.py
+++ tracksrv/licenses/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.contrib.gis.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -6,6 +7,8 @@ class License(models.Model):
   a register of possible open data licenses for use in the depth project
   """
   name = models.CharField(max_length=128, blank=False, null=False)
+  version = models.CharField(max_length=32, blank=True)
+  visited_on = models.DateField(null=False,default=timezone.now)
   url  = models.URLField('link to the license', null=True)
   copy = models.TextField('a copy of the license, in case the original license goes away')
 
--- tracksrv/licenses/views.py
+++ tracksrv/licenses/views.py
@@ -4,7 +4,7 @@ from .models import License
 
 class LicenseViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows vessels to be viewed or edited.
+    API endpoint that allows licenses to be viewed or edited.
     """
     queryset = License.objects.all().order_by('id')
     serializer_class = LicenseSerializer
