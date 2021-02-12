47343148e354d9faf2ff32589aa1a0f8554a4d6f
Wolfgang Schildbach <wschildbach@fermi.franken.de>
2021-02-12 23:07:29 +0100

    disable pagination to better mimic old backend API

--- tracksrv/tracksrv/settings.py
+++ tracksrv/tracksrv/settings.py
@@ -83,8 +83,8 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+#    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#    'PAGE_SIZE': 10,
 }
 
 WSGI_APPLICATION = 'tracksrv.wsgi.application'
