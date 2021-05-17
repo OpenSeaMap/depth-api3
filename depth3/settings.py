12141b6de32fceec1badcc592e53f2f5be24097f
Steffen Volkmann <volkmann@devpool.net>
2021-05-17 12:09:22 +0200

    log: change log level to debug

--- depth3/settings.py
+++ depth3/settings.py
@@ -180,7 +180,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['file'],
-            'level': 'ERROR',
+            'level': 'DEBUG',
             'propagate': True,
         },
     },
